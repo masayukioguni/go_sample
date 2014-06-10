a61fa0d9ec7788022e6dcb9f7776a5ce10fdd6cf
masayuki_oguni <masayuki_oguni@dwango.co.jp>
2014-06-10 16:52:18 +0900

    ディレクトリ構成の変更

--- coreutils/cat/cat.go
+++ coreutils/cat/cat.go
@@ -1,36 +1,30 @@
 package main
 
-import(
-	"os"	
-	"log"
-	"io"
+import (
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
-func main(){
+func main() {
 
 	if len(os.Args) == 1 {
-		wrote_size,err := io.Copy(os.Stdout,os.Stdin)
+		wrote_size, err := io.Copy(os.Stdout, os.Stdin)
 		if err != nil {
 			log.Fatal(err)
 		}
 	} else {
-		for _,fname := range os.Args[1:] {
+		for _, fname := range os.Args[1:] {
 			fd, err := os.Open(fname)
 			if err != nil {
 				log.Fatal(err)
 			}
 
-			wrote_size,err := io.Copy(os.Stdout,fd)
+			wrote_size, err := io.Copy(os.Stdout, fd)
 			if err != nil {
 				log.Fatal(err)
 			}
 		}
 	}
 }
-
-
-
-
-
-
